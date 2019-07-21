9c9ee3dc42df26347f87f57077dccf8baa268b36
jxa8hst <jean_altidor@homdepot.com>
2019-07-20 23:15:36 -0400

    added scroll view

--- src/components/AlbumList/index.js
+++ src/components/AlbumList/index.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Text, StyleSheet, View,
-} from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
+
+import AlbumDetail from '../AlbumDetail';
 
 
 const AlbumList = () => {
   const [rockets, setRockets] = useState([]);
   useEffect(() => {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(res => setRockets(res.data))
+      .then((res) => {
+        setRockets(res.data);
+      })
       .catch(err => console.error(err.message));
   }, []);
-  console.log(rockets);
-  return (
-    <View>
-      <Text>Album List</Text>
-    </View>
+
+  const markup = (
+    <ScrollView>
+      {rockets && rockets.map(album => <AlbumDetail key={album.title} album={album} />)}
+    </ScrollView>
   );
+  return markup;
 };
 
 export default AlbumList;
