ea531fd36c69004800fcff0a4473b0084c824059
jxa8hst <jean_altidor@homdepot.com>
2019-07-20 21:44:22 -0400

    updated lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   extends: 'airbnb',
   rules: {
     "react/jsx-filename-extension": "off",
+    "react/prop-types": "off",
+    "import/prefer-default-export": "off",
     "no-use-before-define": "off"
   }
   
