a6dbba7ff48461dde2f6882a4bfee165fa51dfe6
jxa8hst <jean_altidor@homdepot.com>
2019-07-21 21:19:41 -0400

    firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
 
 import { Header } from './components/common';
 import AlbumList from './components/views/AlbumList';
+import { firebaseConfig } from './config/secret';
 
 
-const App = () => (
-  <Fragment>
-    <View style={{ flex: 1 }}>
-      <Header name="Albums" />
-      <AlbumList />
-    </View>
-  </Fragment>
-);
+const App = () => {
+  useEffect(() => () => {
+    firebase.initializeApp(firebaseConfig);
+  }, []);
+
+  return (
+    <Fragment>
+      <View style={{ flex: 1 }}>
+        <Header name="Albums" />
+        <AlbumList />
+      </View>
+    </Fragment>
+  );
+};
 
 
 export default App;
