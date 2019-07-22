568678f2845810a166f6f051670be45c520470cc
jxa8hst <jean_altidor@homdepot.com>
2019-07-21 21:10:04 -0400

    setting up firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
 
 import { Header } from './components/common';
 import AlbumList from './components/views/AlbumList';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyCpSn19HZKBaNk5hU8E2mXk3vrTlJhBdSU',
+  authDomain: 'auth-5fe11.firebaseapp.com',
+  databaseURL: 'https://auth-5fe11.firebaseio.com',
+  projectId: 'auth-5fe11',
+  storageBucket: '',
+  messagingSenderId: '66320919952',
+  appId: '1:66320919952:web:4468c4a1067857d1',
+};
 
-const App = () => (
-  <Fragment>
-    <View style={{ flex: 1 }}>
-      <Header name="Albums" />
-      <AlbumList />
-    </View>
-  </Fragment>
-);
+
+const App = () => {
+  useEffect(() => {
+    firebase.initializeApp(firebaseConfig);
+  });
+
+  return (
+    <Fragment>
+      <View style={{ flex: 1 }}>
+        <Header name="Albums" />
+        <AlbumList />
+      </View>
+    </Fragment>
+  );
+};
 
 
 export default App;
