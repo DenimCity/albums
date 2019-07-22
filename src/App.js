bea1fe9b079a41b330796e7a1bd1f6fa9da2f1db
Jean Altidor <altidorj1@gmail.com>
2019-07-21 21:16:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,6 @@ import firebase from 'firebase';
 import { Header } from './components/common';
 import AlbumList from './components/views/AlbumList';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCpSn19HZKBaNk5hU8E2mXk3vrTlJhBdSU',
-  authDomain: 'auth-5fe11.firebaseapp.com',
-  databaseURL: 'https://auth-5fe11.firebaseio.com',
-  projectId: 'auth-5fe11',
-  storageBucket: '',
-  messagingSenderId: '66320919952',
-  appId: '1:66320919952:web:4468c4a1067857d1',
-};
-
 
 const App = () => {
   useEffect(() => {
