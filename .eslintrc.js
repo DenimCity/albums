e852c23c662d3ca2b18cf3c6c715e06ce2696eb1
jxa8hst <jean_altidor@homdepot.com>
2019-07-27 18:31:21 -0400

    added arrow-body-style

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
     "import/prefer-default-export": "off",
     "camelcase": "off",
     "react/destructuring-assignment": "off",
-    "no-use-before-define": "off"
+    "no-use-before-define": "off",
+    "arrow-body-style": "off"
   }
   
 };
